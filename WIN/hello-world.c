c898831d05217c8f8f53d5474984a274fe3fde6c
Neal Gray <ngray@graylabs.net>
2018-11-30 14:39:46 -0500

    Adding WIN Hello World to Repo

---
+++ WIN/hello-world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <io.h>
+#include <windows.h>
+
+int main()
+{
+    printf("Hello, World! This is a native C program compiled on the command line.\n");
+    return 0;
+} 
